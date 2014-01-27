8a30617c2aaff370a5173a9cf8ab075b5e193c9e
Jonah Kirangi <jonahkirangi@gmail.com>
2014-01-26 21:18:59 -0800

    Sign in test passing

--- test/features/auth/sign_out_test.rb
+++ test/features/auth/sign_out_test.rb
@@ -1,20 +1,20 @@
-# require "test_helper"
+require "test_helper"
 
-# feature "As a site visitor, I want to be able to sign out" do
-#   scenario "signing out of the site" do
+feature "As a site visitor, I want to be able to sign out" do
+  scenario "signing out of the site" do
 
-#     # SIGN IN an existing user
-#     visit user_session_path
+    # SIGN IN an existing user
+    visit user_session_path
 
-#     fill_in "Email", with: "name@example.com"
-#     fill_in "Password", with: "password"
-#     page.find("[type='submit']").click
+    fill_in "Email", with: "dad@aol.com"
+    fill_in "Password", with: "letmein"
+    save_and_open_page
+    click_on "Sign in"
 
-#     # SIGN OUT the existing user
-#     visit root_path
-#     click_on "Sign Out"
+    # SIGN OUT the existing user
+    #click_on "Sign out"
 
-#     # VERIFY user is signed out
-#     page.must_have_content "Signed out successfully."
-#   end
-# end
+    # VERIFY user is signed out
+    #page.must_have_content "Signed out"
+  end
+end
--- test/features/can_access_home_test.rb
+++ test/features/can_access_home_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-feature "CanAccessHome" do
+feature "As a user, I want to access the home page" do
   scenario "index is displayed" do
     visit root_path
     page.must_have_content "Welcome aboard"
