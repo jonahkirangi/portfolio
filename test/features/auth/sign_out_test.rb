dc2d74074c306424d5a54a5eb40f60b2f8b1d04f
Jonah Kirangi <jonahkirangi@gmail.com>
2014-01-27 19:50:23 -0800

    Sign out test passing

--- test/features/auth/sign_out_test.rb
+++ test/features/auth/sign_out_test.rb
@@ -4,17 +4,20 @@ feature "As a site visitor, I want to be able to sign out" do
   scenario "signing out of the site" do
 
     # SIGN IN an existing user
-    visit user_session_path
+    visit new_user_session_path
+    page.text.must_include "Remember me"
 
-    fill_in "Email", with: "dad@aol.com"
-    fill_in "Password", with: "letmein"
-    save_and_open_page
-    click_on "Sign in"
+    fill_in "Email", with: users(:User1).email
+    fill_in "Password", with: "password"
+    click_button "Sign in"
+
+    page.text.must_include "Signed in successfully."
 
     # SIGN OUT the existing user
-    #click_on "Sign out"
+    click_on "Sign Out"
 
     # VERIFY user is signed out
-    #page.must_have_content "Signed out"
+    page.must_have_content "Signed out"
+
   end
 end
