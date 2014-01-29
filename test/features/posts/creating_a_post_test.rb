b2abd1c08bba58e29c788077e9a59c092cce6fd2
Jonah Kirangi <jonahkirangi@gmail.com>
2014-01-28 17:15:53 -0800

    Authorization passing

--- test/features/posts/creating_a_post_test.rb
+++ test/features/posts/creating_a_post_test.rb
@@ -24,5 +24,54 @@ feature "Creating A Post" do
     page.text.must_include users(:User1).email
   end
 
+  scenario "authors can't publish" do
+    sign_in(:author)
+    visit new_post_path
+    page.wont_have_field('Published')
+  end
+
+  scenario "unauthenticated site visitors cannot visit new_post_path" do
+    visit new_post_path
+    page.must_have_content "You need to sign in or sign up before continuing"
+  end
+
+  scenario "unauthenticated site vistiors cannot see new post button" do
+    # When I visit the blog index page
+    visit posts_path
+    # Then I should not see the "New Post" button
+    page.wont_have_link "New Post"
+  end
+
+  scenario "authors can't publish" do
+    # Given an author's account
+    sign_in(:author)
+
+    # When I visit the new page
+    visit new_post_path
+
+    # There is no checkbox for published
+    page.wont_have_field('published')
+  end
+
+  scenario "editors can publish" do
+    # Given an editor's account
+    sign_in(:editor)
+
+    # When I visit the new page
+    visit new_post_path
+
+    # There is a checkbox for published
+    page.must_have_field('Published')
+
+    # When I submit the form
+    fill_in "Title", with: posts(:cr).title
+    fill_in "Body", with: posts(:cr).body
+    check "Published"
+    click_on "Create Post"
+
+    # Then the published post should be shown
+    page.text.must_include "Status: Published"
+  end
+
 end
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -26,3 +26,10 @@ class ActionDispatch::IntegrationTest
 
   include Capybara::DSL
 end
+
+def sign_in(role = :editor)
+    visit new_user_session_path
+    fill_in "Email", with: users(role).email
+    fill_in "Password", with: "password"
+    click_on "Sign in"
+end
